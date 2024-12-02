67750db40a8e6b3cd22e289b4d5c1a621684017a
ebulku <endri.bulku@live.com>
2024-12-02 01:37:04 +0100

    Add Register and login Links

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,10 +1,16 @@
-import { LoginForm } from '@/components/login-form';
+import { LoginForm } from "@/components/login-form";
+import Link from "next/link";
 
 export default function LoginPage() {
   return (
     <div className="min-h-[80vh] flex items-center justify-center">
       <div className="w-full max-w-md">
         <LoginForm />
+        <div className="text-right mt-4 text-sm">
+          <Link href="/register" className="hover:underline">
+            Don't have an account? Register here
+          </Link>
+        </div>
       </div>
     </div>
   );
--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,10 +1,16 @@
-import { RegisterForm } from '@/components/register-form';
+import { RegisterForm } from "@/components/register-form";
+import Link from "next/link";
 
 export default function LoginPage() {
   return (
     <div className="min-h-[80vh] flex items-center justify-center">
       <div className="w-full max-w-md">
         <RegisterForm />
+        <div className="text-right mt-4 text-sm">
+          <Link href="/login" className="hover:underline">
+            Already have an account? Login
+          </Link>
+        </div>
       </div>
     </div>
   );
